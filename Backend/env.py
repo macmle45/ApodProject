14d910c960a885388d410f7ded9af7d15075f261
Maciej Mleczko <maciej.mleczko.net@gmail.com>
2022-02-02 10:01:28 +0100

    update | wrapped methods to class and set as static methods

--- Backend/env.py
+++ Backend/env.py
@@ -4,25 +4,27 @@ import os
 import getpass
 
 
-def load_env():
-    load_dotenv('Backend/ConfigFiles/.env')
+class Env:
+    @staticmethod
+    def load_env():
+        load_dotenv('Backend/ConfigFiles/.env')
 
-    env_variables = dict()
-    env_variables['API_KEY'] = os.getenv('API_KEY')
+        env_variables = dict()
+        env_variables['API_KEY'] = os.getenv('API_KEY')
 
-    temp_path = os.getenv('CONFIG_FILE_PATH')
-    env_variables['CONFIG_FILE_PATH'] = temp_path.replace('_user_', getpass.getuser())
+        temp_path = os.getenv('CONFIG_FILE_PATH')
+        env_variables['CONFIG_FILE_PATH'] = temp_path.replace('_user_', getpass.getuser())
 
-    return env_variables
+        return env_variables
 
+    @staticmethod
+    def get_config(filepath, section):
+        parser = ConfigParser()
+        parser.read(filepath)
 
-def get_config(filepath, section):
-    parser = ConfigParser()
-    parser.read(filepath)
+        if parser.has_section(section):
+            params = dict(parser.items(section))
+        else:
+            raise Exception(f'Section {section} not found in config file.')
 
-    if parser.has_section(section):
-        params = dict(parser.items(section))
-    else:
-        raise Exception(f'Section {section} not found in config file.')
-
-    return params
+        return params

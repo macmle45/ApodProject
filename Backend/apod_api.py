6cb6fb9e358a78efe238352cdb46de6cfde64a06
Maciej Mleczko <maciej.mleczko.net@gmail.com>
2022-02-02 10:49:04 +0100

    update | added 'retrieve_json_data' static method

--- Backend/apod_api.py
+++ Backend/apod_api.py
@@ -1,4 +1,5 @@
 import requests as req
+import json
 
 
 class ApodAPI:
@@ -14,3 +15,20 @@ class ApodAPI:
             return result
         else:
             raise ConnectionError(f'Error was thrown during downloading data from API\nStatus code: {status}')
+
+    @staticmethod
+    def retrieve_json_data(json_data):
+        apod_data = dict()
+
+        apod_data['date'] = json_data['date']
+        apod_data['title'] = json_data['title']
+        apod_data['description'] = json_data['explanation']
+        apod_data['media_type'] = json_data['media_type']
+        apod_data['media_hdurl'] = json_data['hdurl']
+
+        try:
+            apod_data['copyright'] = json_data['copyright']
+
+            return apod_data
+        except KeyError:
+            return apod_data

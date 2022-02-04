3fcd5202f2982cc9f688993cbc4b2bdd4129d3d8
Maciej Mleczko <maciej.mleczko.net@gmail.com>
2022-02-04 19:11:38 +0100

    updated 'main.py' file | environment configuration, dashboard initialization

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from Backend.env import Env
+from Frontend.dashboard import Dashboard
+
+
 def main():
-    pass
+    env_variables = Env.load_env()
+    api_key = env_variables['API_KEY']
+    config_file = env_variables['CONFIG_FILE_PATH']
+
+    configs = Env.get_config(filepath=config_file, section='api')
+    url = configs['url']
+
+    dashboard = Dashboard()
+    dashboard.init_dashboard()
 
 
 if __name__ == '__main__':

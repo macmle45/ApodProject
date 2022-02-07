402ef56c5fab9983869b8e2434830018a8439d25
Maciej Mleczko <maciej.mleczko.net@gmail.com>
2022-02-07 11:07:07 +0100

    removed section with loading environment variables and moved to 'dashboard' class

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from Backend.env import Env
 from Frontend.dashboard import Dashboard
 
 
 def main():
-    env_variables = Env.load_env()
-    api_key = env_variables['API_KEY']
-    config_file = env_variables['CONFIG_FILE_PATH']
-
-    configs = Env.get_config(filepath=config_file, section='api')
-    url = configs['url']
-
     dashboard = Dashboard()
     dashboard.init_dashboard()
 
